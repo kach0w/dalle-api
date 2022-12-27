0800dc438be2f83e184ec484985904d5f8d3a082
kach0w <karsab343@gmail.com>
2022-12-27 11:48:32 -0800

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -19,19 +19,25 @@ export default async function (req, res) {
   if (animal.trim().length === 0) {
     res.status(400).json({
       error: {
-        message: "Please enter a valid animal",
+        message: "Please enter a valid prompt",
       }
     });
     return;
   }
 
   try {
-    const completion = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: generatePrompt(animal),
-      temperature: 0.6,
+    
+    const completion = await openai.createImage({
+        prompt: animal,
+        n: 1,
+        size: "1024x1024",
     });
-    res.status(200).json({ result: completion.data.choices[0].text });
+    // const completion = await openai.createCompletion({
+    //   model: "text-davinci-003",
+    //   prompt: generatePrompt(animal),
+    //   temperature: 0.6,
+    // });
+    res.status(200).json({ result: completion.data.data[0].url });
   } catch(error) {
     // Consider adjusting the error handling logic for your use case
     if (error.response) {
@@ -52,7 +58,6 @@ function generatePrompt(animal) {
   const capitalizedAnimal =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
   return `Suggest three names for an animal that is a superhero.
-
 Animal: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
 Animal: Dog
