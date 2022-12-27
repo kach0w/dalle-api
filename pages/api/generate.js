4b7866b206e1c8807b37a2db4b8593ccb60a2414
kach0w <karsab343@gmail.com>
2022-12-27 12:08:48 -0800

    Update generate.js

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
